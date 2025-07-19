8c0f122ea1c4a6e655524bf28f994502f751e5c1
Moustafa Laabassi <laabassi.moustafa7@gmail.com>
2025-07-19 07:06:26 +0000

    feat: get username input

--- number_guess.sh
+++ number_guess.sh
@@ -6,4 +6,16 @@ TRIES=0
 NUMBER=$(( RANDOM % 1000 + 1 ))
 
 echo "Enter your username:"
-read USERNAME
+read NAME
+
+USER_INFO=$($PSQL "SELECT * FROM users WHERE username='$NAME'")
+
+if [[ -z $USER_INFO ]]
+then
+  echo "Welcome, $NAME! It looks like this is your first time here."
+  INSERT_NEW_USER=$($PSQL "INSERT INTO users(username, games_played) VALUES('$NAME', 0)")
+else
+  IFS='|' read USERID USERNAME GAMESPLAYED <<< "$USER_INFO"
+  BESTGAME=$($PSQL "SELECT MIN(guesses_amount) FROM games WHERE player_id=$USERID")
+  echo "Welcome back, $NAME! You have played $GAMESPLAYED games, and your best game took $BESTGAME guesses."
+fi
